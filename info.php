8d226a10c11040116226b1dd3d1082ae66501f9a
alpizano <apizano@purdue.edu>
2019-10-12 11:19:05 +0000

    created test database and pulling mock data from MySQL db and displaying to info.php

--- info.php
+++ info.php
@@ -1,3 +1,32 @@
+<html>
+<head>
+<title>MySQL DB Test</title>
+<body>
 <?php
-phpinfo();
+ 
+$dbHost = 'localhost';
+$dbUser = 'root';
+$dbPass = '!Supernova4k';
+$dbName = "test";
+$conn = mysqli_connect($dbHost,$dbUser,$dbPass,$dbName);
+
+
+
+$sql = "SELECT * FROM students;";
+$result = mysqli_query($conn,$sql);
+$resultCheck = mysqli_num_rows($result);
+echo $resultCheck . "<br>";
+
+if($resultCheck > 0) {
+while($row = mysqli_fetch_assoc($result)) {
+echo $row['id'] . $row['name'] . $row['age'] . "<br>";
+}
+}
+
+
+
+
+mysql_close($conn);
 ?>
+</body>
+</html>
